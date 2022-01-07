b72d929d7e825a69c3b52b693c3e74806c7838d3
ViliBolkovac <vilibolkovac@gmail.com>
2022-01-07 18:33:59 +0100

    navbar animation

--- index.php
+++ index.php
@@ -6,9 +6,26 @@
         <title>Vex</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="stylesheet.css">
+        <link href='https://css.gg/briefcase.css' rel='stylesheet'>
+        <link href='https://css.gg/credit-card.css' rel='stylesheet'>
+        <link href='https://css.gg/attribution.css' rel='stylesheet'>
+        <link href='https://css.gg/profile.css' rel='stylesheet'>
+        <link href='https://css.gg/log-off.css' rel='stylesheet'>
+        <link href='https://css.gg/info.css' rel='stylesheet'>
     </head>
     <body>
+        <nav>
+            <ul>
+                <li><a href=""><i class="gg-profile"></i></a></li>
+                <li><a href=""><i class="gg-briefcase"></i></a></li>
+                <li><a href=""><i class="gg-credit-card"></i></a></li>
+                <li><a href=""><i class="gg-attribution"></i></a></li>
+                <li><a href=""><i class="gg-info"></i></a></li>
+                <li><a href="" class="logoutbtn"><i class="gg-log-off"></i></i></a></li>
+            </ul>
+        </nav>
 
+    <h1></h1>
         <script src="" async defer></script>
     </body>
 </html>
