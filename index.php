c9e6f842cf2d6dec578e3d71a88e655eb27da528
ViliBolkovac <vilibolkovac@gmail.com>
2022-01-07 18:36:17 +0100

    navbar animation

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Vex</title>
+        <title>Druga stranica</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="stylesheet.css">
         <link href='https://css.gg/briefcase.css' rel='stylesheet'>
